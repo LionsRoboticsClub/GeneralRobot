9823a747093b62c50a87de3c5460399c1226c106
unknown <pcruiher089@gmail.com>
2019-08-14 11:32:03 -0500

    fixed com

--- main/main.ino
+++ main/main.ino
@@ -8,25 +8,27 @@ int unproccesed_data;
 
 template <typename T>
 void LogData(T data){
-  Serial1.println(data);
+  Serial.println(data);
 }
 
 int InputData(){
   int data = 0;
-  Serial1.println("This was called");
-  if(Serial1.available() > 0) {
-    data = Serial1.read();
-    LogData(data);
-  }
+  Serial.println("This was called");
+  data = Serial1.read();
+  LogData(data);
+
   return data;
 }
 
 Buzzer buzz(28);
 void setup(){
+  Serial.begin(9600);
   Serial1.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
 }
 void loop()
 {
+
+if(Serial1.available())
 unproccesed_data = InputData();                    
  
 }   
