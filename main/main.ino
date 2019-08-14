8a3c821c78594263beeb5853ee47f4e52b99b20b
Paco <paco.sanchez.ram@gmail.com>
2019-08-14 10:17:31 -0500

    A ver si jala

--- main/main.ino
+++ main/main.ino
@@ -12,8 +12,9 @@ void LogData(T data){
 
 int InputData(){
   int data = 0;
+  Serial1.println("This was called");
   if(Serial1.available() > 0) {
-     data = Serial1.read();
+    data = Serial1.read();
     LogData(data);
   }
   return data;
@@ -25,6 +26,6 @@ void setup(){
 }
 void loop()
 {
-                          
+unproccesed_data = InputData();                    
  
 }   
