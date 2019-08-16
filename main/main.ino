16a8d08b2cd612824597201825ff791b7709285a
unknown <pcruiher089@gmail.com>
2019-08-15 22:19:01 -0500

    moved mecanum to processing

--- main/main.ino
+++ main/main.ino
@@ -18,7 +18,7 @@ Motor motor1(7,6,0,0);
 Motor motor2(4,5,0,0);
 Motor motor3(9,8,0,0);
 Motor motor4(3,2,0,0);
-MecanumDrive mecanum(motor1, motor2, motor3, motor4, 0,0,0);
+//MecanumDrive mecanum(motor1, motor2, motor3, motor4, 0,0,0);
 Servo band; 
 Servo intake; 
 Buzzer buzz(28);
@@ -26,8 +26,8 @@ Buzzer buzz(28);
 bool a = false, b = false, x = false, y = false; 
 
 void setup(){
-  band.attach(29);
-  intake.attach(30);
+  //band.attach(29);
+  //intake.attach(30);
   Serial.begin(9600);
   Serial1.begin(9600);
 }
@@ -37,12 +37,21 @@ void loop(){
       movement_data[current_index] = Serial1.read();
       current_index == 4 ? current_index = 0 : ++current_index;
     }
+    Serial.println("DATA");
+    Serial.println(movement_data[0]);
+    Serial.println(movement_data[1]);
+    Serial.println(movement_data[2]);
+    Serial.println(movement_data[3]);
     actualData[0] = movement_data[0] / 128.0;
     actualData[1] = movement_data[1] / 128.0;
     actualData[2] = movement_data[2] * 3.0;
     actualData[3] = movement_data[3] * 1.0;
-
-    mecanum.moveTowards(actualData[2],actualData[0],0);
-    intake.writeMicroseconds(1500);
-    band.writeMicroseconds(1500);
+    motor1.move((movement_data[0]));
+    motor2.move((movement_data[1]));
+    motor3.move((movement_data[2]));
+    motor4.move((movement_data[3]));
+    
+    //mecanum.moveTowards(actualData[2],actualData[0],0);
+    //intake.writeMicroseconds(1500);
+    //band.writeMicroseconds(1500);
 }   
